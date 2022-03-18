16b052195cd22c22b1d42d941ba9fa818cf32c5d
Iurie Manea <manea.iurie@gmail.com>
2022-03-18 20:30:35 +0200

    Show Bearn as ViewController

--- DemoBearnSDK/ViewController.swift
+++ DemoBearnSDK/ViewController.swift
@@ -36,5 +36,28 @@ class ViewController: UIViewController {
                              externalId: "12121212121212121211",
                              token: "11ffa303-6c84-11ea-adfe-0e4cb9ff1234")
     }
+    
+    @IBAction func btnAsViewControllerWithFederatedTouch(_ sender: Any) {
+        
+        Bearn.shared.loadCustomInfoFrom(bundle: .main, localizationStringsFile: "BearnStrings", localizationColorsFile: "BearnColors.json")
+        
+        // Create instance of Bearn View Controller with federated login
+        let bearnVC = Bearn.shared.bearnViewController(isExitHidden: false, // Set isExitHidden to "true" if you do not need the Exit button
+                                                       // set parterId to nil or empty string if need to dispaly Bearn Login screen
+                                                       parterId: "22ffa303-6c84-11ea-adfe-0e4cb9ff8a2a",
+                                                       firstName: "John4",
+                                                       lastName: "Doe4",
+                                                       email: "john.doe4@testmail.com",
+                                                       externalId: "121212121212121212",
+                                                       token: "11ffa303-6c84-11ea-adfe-0e4cb9ff1234",
+                                                       // Set onClose to nil in case exit is not visible
+                                                       onClose: { bearnViewController in
+            // Calls when the Exit button is pressed, if visible.
+            bearnViewController?.dismiss(animated: true, completion: nil)
+        })
+        
+        // Present bearn on current ViewController
+        self.present(bearnVC, animated: true, completion: nil)
+    }
 }
 
